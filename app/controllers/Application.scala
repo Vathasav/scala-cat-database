a7254feb19a3c3598383fd8233193d361f26ff44
vathasav <sriharsha1250@gmail.com>
2015-07-23 13:06:36 +0300

    Added functionality to directly save into database

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -7,9 +7,6 @@ import play.api.data._
 import play.api.data.Forms._
 import play.api.Play.current
 import play.api.i18n.Messages.Implicits._
-//import play.api.mvc.MultiPartFormData.FilePart_
-
-//import anorm._
 
 
 import models._
@@ -19,11 +16,14 @@ import Gender._
 import scala.io._
 import java.io._
 
-
+/**
+ * Manage a database of cats
+ */
 class Application extends Controller {
     
-   
-
+   /**
+   * Describe the cat form used in both add and edit screens.
+   */
  val catForm: Form[Cat] = Form(
      mapping(
         "id" -> ignored(None:Option[Long]),
@@ -36,27 +36,44 @@ class Application extends Controller {
         "picture"->ignored(Option.empty[Array[Byte]])
      )(Cat.apply)(Cat.unapply)
 )
- 
+
+   // -- Actions
+
+  /**
+   * Handle default path requests, redirect to cats list
+   */  
   def index = Action {
  
       Redirect(routes.Application.cats)
 
   }
  
+ /**
+   * Display list of cats in the database
+   */
   def cats = Action {
     Ok(views.html.index(Cat.all()))
 }
 
+  /**
+   * Display new cat form
+   */
   def create = Action {
       Ok(views.html.addcat(catForm))
   }
   
+  /**
+   * Handle deletion of cat from database
+   */
   def deleteCat(id: Long) = Action {
     Cat.delete(id)
     println(" Cat with "+id + "has been deleted")
     Redirect(routes.Application.cats)
   }
   
+  /**
+   * Display the editform of an existing cat
+   */
   def editCat (id: Long) = Action {
       Cat.findById(id).map { cat =>
       Ok(views.html.editcat(cat, catForm.fill(cat)))
@@ -65,41 +82,53 @@ class Application extends Controller {
   
   }
   
+  /**
+   * Handle edit form submission
+   * 
+   */
   def updateCat (id: Long) = Action (parse.multipartFormData) {
-      
-      implicit request =>
-  request.body.file("imageFile").map { picture =>
-    // retrieve the image and put it where you want...
     
-    val filename = picture.filename
+    // if picture is available use it for updating cat 
+    // otherwise update cat with previous picture (if available)  
+      implicit request =>
+    request.body.file("imageFile").map { picture =>
   
-    val imageFile = new java.io.File(filename)
-    picture.ref.moveTo(imageFile)
   
-    val bis = new BufferedInputStream(new FileInputStream(imageFile))
+   // load the image into binary array -- obtained from play documentation --
+    val bis = new BufferedInputStream(new FileInputStream(picture.ref.file))
     val bArray = Stream.continually(bis.read).takeWhile(-1 !=).map(_.toByte).toArray
-    // handle the other form data
+    
+    // if form is bad return badrequest else process the data
     catForm.bindFromRequest.fold(
       formWithErrors => BadRequest(views.html.editcat(Cat.findById(id).get,formWithErrors)),
 
    cat => {
       
+      // copy new cat from old cat but with new picture
       val newCat = cat.copy(picture = Some(bArray))
       
+      //update cat
       Cat.update(id, newCat)
+      
       println("update cat wit image"+cat.name)
+      
+      //redirect to front page
       Redirect(routes.Application.cats)
     }
     )
   }.getOrElse(
       
+      // if no picture is found update cat with previous picture
+      
      catForm.bindFromRequest.fold(
       formWithErrors => BadRequest(views.html.editcat(Cat.findById(id).get, formWithErrors)),
 
    cat => {
-       
+      
+     // get cat from DB
      val catfromDB: Cat = Cat.findById(id).get
      
+     //Add old cat's picture to new cat if aavailable and update in DB
      if(!catfromDB.picture.isEmpty){
          val newcat = cat.copy(picture=Cat.findById(id).get.picture)
          Cat.update(id, newcat)
@@ -110,6 +139,7 @@ class Application extends Controller {
       
       
       println("added cat - without image"+cat.name)
+      
       Redirect(routes.Application.cats)
     }
     )
@@ -120,6 +150,10 @@ class Application extends Controller {
       
   }
   
+  
+  /**
+   * return image for a cat
+   */
   
   def getImage(id:Long) = Action{
   
@@ -142,36 +176,47 @@ class Application extends Controller {
   }
     
     
-  
-def newCat = Action(parse.multipartFormData) { implicit request =>
-  request.body.file("imageFile").map { picture =>
-    // retrieve the image and put it where you want...
+  /**
+   * Handle the new cat form submission
+   * 
+   * if picture is available use it
+   * otherwise insert cat without picture
+   */
+    def newCat = Action(parse.multipartFormData) { implicit request =>
     
-    val imageFile = new java.io.File("myFileName")
-    picture.ref.moveTo(imageFile)
+    //if picture is available add it
+     request.body.file("imageFile").map { picture =>
     
     
-    val bis = new BufferedInputStream(new FileInputStream(imageFile))
+   // load the image into binary array --obtained from play documentation--
+    val bis = new BufferedInputStream(new FileInputStream(picture.ref.file))
     val bArray = Stream.continually(bis.read).takeWhile(-1 !=).map(_.toByte).toArray
-    // handle the other form data
+    
     catForm.bindFromRequest.fold(
       formWithErrors => BadRequest(views.html.addcat(formWithErrors)),
 
    cat => {
-      
-      Cat.create(cat, Some(bArray))
+       
+      // update newcat from form data but with new picture
+      val addCat = cat.copy(picture = Some(bArray))
+       
+      // insert cat into DB
+      Cat.create(addCat) //, Some(bArray))
+     
       println("added cat"+cat.name)
       Redirect(routes.Application.cats)
     }
     )
   }.getOrElse(
       
+     // if no picture is found add cat to DB without image data
+      
      catForm.bindFromRequest.fold(
       formWithErrors => BadRequest(views.html.addcat(formWithErrors)),
 
    cat => {
       
-      Cat.create(cat, None)
+      Cat.create(cat)
       println("added cat"+cat.name)
       Redirect(routes.Application.cats)
     }
@@ -181,18 +226,5 @@ def newCat = Action(parse.multipartFormData) { implicit request =>
       )
 }
 
-
-
-  
-  
- 
-  
-/*  def deleteCat(id: Long) = Action {
-    //Cat.delete(id)
-    
-    val bis = new BufferedInputStream(new FileInputStream(fileName))
-val bArray = Stream.continually(bis.read).takeWhile(-1 !=).map(_.toByte).toArray
-  }
-  */
 
 }
--- app/models/Cat.scala
+++ app/models/Cat.scala
@@ -5,27 +5,22 @@ import anorm.SqlParser._
 import play.api.db._
 import play.api.Play.current
 
-/*object Gender extends Enumeration {
-    val male, female = Value
-
-    type Gender = Value
-}*/
 
 
-object Gender {
-    
-    val list = List("Male","Female")
-}
 
+
+// defining cat model
 case class Cat(id: Option[Long] = None, name: String, color: String, race: String, gender: String, picture: Option[Array[Byte]])
 
 
-
+//object for managing cat operations
 object Cat {
   
  
   
-  
+ /**
+  *Parser for transforming a ResultSet to Cat value 
+  */
   
   val cat = {
   get[Option[Long]]("id") ~ 
@@ -38,16 +33,21 @@ object Cat {
   }
 }
 
-def all(): List[Cat] = DB.withConnection { implicit connection =>
+/**
+  *Lists all cats in the database
+  * Returns List[Cat]
+  */
+
+  def all(): List[Cat] = DB.withConnection { implicit connection =>
   SQL("select * from cat").as(cat *)
-}
+  }
 
   /**
    * Insert a new cat.
    *
    * @param cat The values of new cat.
    */
-  def create(cat: Cat, bArray: Option[Array[Byte]]) = {
+  def create(cat: Cat) = {
     DB.withConnection { implicit connection =>
       SQL(
         "insert into cat values ((select next value for cat_id_seq), {name}, {color}, {race}, {gender}, {picture})"
@@ -56,7 +56,7 @@ def all(): List[Cat] = DB.withConnection { implicit connection =>
         'color -> cat.color,
         'race -> cat.race,
         'gender -> cat.gender,
-        'picture -> bArray
+        'picture -> cat.picture
       ).executeUpdate()
     }
     
@@ -117,10 +117,16 @@ def all(): List[Cat] = DB.withConnection { implicit connection =>
         
     }
     
-    // collected from web
+    /**
+     * List of colors collected from web
+     */ 
     object Colors{
         
         val list = List ("aliceblue", "antiquewhite", "aqua", "aquamarine", "azure", "beige", "bisque", "black", "blanchedalmond", "blue", "blueviolet", "brown", "burlywood", "cadetblue", "chartreuse", "chocolate", "coral", "cornflowerblue", "cornsilk", "crimson", "cyan", "darkblue", "darkcyan", "darkgoldenrod", "darkgray", "darkgreen", "darkkhaki", "darkmagenta", "darkolivegreen", "darkorange", "darkorchid", "darkred", "darksalmon", "darkseagreen", "darkslateblue", "darkslategray", "darkturquoise", "darkviolet", "deeppink", "deepskyblue", "dimgray", "dodgerblue", "firebrick", "floralwhite", "forestgreen", "fuchsia", "gainsboro", "ghostwhite", "gold", "goldenrod", "gray", "green", "greenyellow", "honeydew", "hotpink", "indianred", "indigo", "ivory", "khaki", "lavender", "lavenderblush", "lawngreen", "lemonchiffon", "lightblue", "lightcoral", "lightcyan", "lightgoldenrodyellow", "lightgreen", "lightgrey", "lightpink", "lightsalmon", "lightseagreen", "lightskyblue", "lightslategray", "lightsteelblue", "lightyellow", "lime", "limegreen", "linen", "magenta", "maroon", "mediumaquamarine", "mediumblue", "mediumorchid", "mediumpurple", "mediumseagreen", "mediumslateblue", "mediumspringgreen", "mediumturquoise", "mediumvioletred", "midnightblue", "mintcream", "mistyrose", "moccasin", "navajowhite", "navy", "oldlace", "olive", "olivedrab", "orange", "orangered", "orchid", "palegoldenrod", "palegreen", "paleturquoise", "palevioletred", "papayawhip", "peachpuff", "peru", "pink", "plum", "powderblue", "purple", "red", "rosybrown", "royalblue", "saddlebrown", "salmon", "sandybrown", "seagreen", "seashell", "sienna", "silver", "skyblue", "slateblue", "slategray", "snow", "springgreen", "steelblue", "tan", "teal", "thistle", "tomato", "turquoise", "violet", "wheat", "white", "whitesmoke", "yellow", "yellowgreen")
     }
     
-
+    // list for Gender
+    object Gender {
+    
+        val list = List("Male","Female")
+    }
